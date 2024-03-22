b367bdfdd4b7d68091c6dd976ebd4e756cef8467
Alex <a.perekrestov@gmail.com>
2024-03-22 12:54:09 +0300

    Изоляция CSS модулей

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import './App.scss'
+import React, { useState } from 'react';
+import classes from './App.module.scss';
 
 export const App = () => {
 	const [count, setCount] = useState<number>(0)
@@ -8,8 +8,13 @@ export const App = () => {
 
 	return (
 		<div>
-			<h1>{count}</h1>
-			<button onClick={increment}>inc <span>+1</span></button>
+			<h1 className={classes.value}>{count}</h1>
+			<button
+				className={classes.button}
+				onClick={increment}
+			>
+				inc <span>+1</span>
+			</button>
 		</div>
 	)
 }
